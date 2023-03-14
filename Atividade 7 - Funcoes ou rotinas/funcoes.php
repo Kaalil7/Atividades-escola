47dd9e2f9d774e2f8f7e335bd873d10a1abc87b5
Henrique Kalil <94183276+Kaalil7@users.noreply.github.com>
2023-03-13 21:51:53 -0300

    Aprendendo-funcoes
    Aprendendo funções e rotinas.

---
+++ Atividade 7 - Funcoes ou rotinas/funcoes.php
@@ -0,0 +1,11 @@
+<?php
+
+    function nome(){
+        echo("<p>Henrique Kalil S. Garcia</p>");
+    }
+
+    function idade(){
+        echo(16);
+        echo("Programa finalizado");
+    }
+?>
---
+++ Atividade 7 - Funcoes ou rotinas/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 7 - Funções ou rotinas</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div class="box">
+        <h1>Atividade 7 - Funções ou rotinas</h1>
+    <div class="text1">
+    <?php
+    include "funcoes.php";
+
+    nome();
+    idade();
+    ?>
+    </div>
+    </div>
+</body>
+</html>
